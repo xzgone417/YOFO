e11d876db63297d59d439221a3638d46b7ecf888
xzgone417 <2471671292@qq.com>
2022-05-11 16:12:42 +0800

    徐正罡增加啊了路由

---
+++ router/index.js
@@ -0,0 +1,62 @@
+// 引用模板和中间件
+const express = require("express");
+// 导入model中的js例如
+const Test = require("../model/test")
+
+// 挂载中间件
+const path = require("path")
+const multer = require('multer');
+const mongoose = require("mongoose");
+const moment = require("moment")
+const router = express.Router();
+
+// 渲染页面
+// aboutUs
+router.get("/", (req, res) => {
+    res.render("index.html")
+})
+router.get("/about", (req, res) => {
+    res.render("aboutUs/about.html")
+})
+router.get("/contact", (req, res) => {
+    res.render("aboutUs/contact.html")
+})
+router.get("/honor", (req, res) => {
+    res.render("aboutUs/honor.html")
+})
+router.get("/joinus", (req, res) => {
+    res.render("aboutUs/joinus.html")
+})
+router.get("/milestone", (req, res) => {
+    res.render("aboutUs/milestone.html")
+})
+// cases
+router.get("/cases", (req, res) => {
+    res.render("cases/cases.html")
+})
+// CT
+router.get("/autodon", (req, res) => {
+    res.render("CT/autodon.html")
+})
+router.get("/dubhewer", (req, res) => {
+    res.render("CT/dubhewer.html")
+})
+router.get("/pirox", (req, res) => {
+    res.render("CT/pirox.html")
+})
+// news
+router.get("/news", (req, res) => {
+    res.render("newsCenter/news.html")
+})
+router.get("/exhibition", (req, res) => {
+    res.render("newsCenter/exhibition.html")
+})
+// support
+router.get("/support", (req, res) => {
+    res.render("support/support.html")
+})
+// technology
+router.get("/technology", (req, res) => {
+    res.render("technology/technology.html")
+})
+module.exports = router;
--- router/news.js
+++ router/news.js
@@ -1,18 +1,19 @@
 // 引用模板和中间件
 const express = require("express");
+// 导入model中的js例如
+const Test = require("../model/test")
+
+// 挂载中间件
 const path = require("path")
 const multer = require('multer');
 const mongoose = require("mongoose");
 const moment = require("moment")
+const router = express.Router();
 
-const testm = require("../model/test")
-
-const test = express.Router();
-
+// newsCenter里面的news文件夹下面的子页面
 
 
 
 
 
-
-module.exports = test;
+module.exports = router;
